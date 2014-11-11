a4d68f452fdae7fc2f9ff6ba3dc1fcdee58fe723
Kiko <mmhabib11@gmail.com>
2014-11-11 00:05:13 -0500

    Some bug fixes. Added user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 	has_many :products, through: :reviews
 	has_secure_password
 	validates :name, presence: true
+	validates :email, presence: true
+	validates :email, format: {with: /\@/, message: " format is incorrect. Should be your_email@domain.com"}
 end
