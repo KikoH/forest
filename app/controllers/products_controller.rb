61f0be6eaa04af57710c6acae68d7be0e7ca1dad
Kiko <mmhabib11@gmail.com>
2014-11-25 16:04:40 -0500

    Add logic to search in products controller index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all
+    @products = if params[:search]
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+    else
+      Product.all
+    end
+
+    if request.xhr?
+      render :partial => 'products'
+    end
     @categories = Category.all
   end
 
@@ -10,49 +18,49 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	@product = Product.find(params[:id])
+   @product = Product.find(params[:id])
 
-  	if current_user
-  		@review = @product.reviews.build
-  	end
+   if current_user
+    @review = @product.reviews.build
   end
+end
 
-  def new
-  	@product = Product.new
-  end
+def new
+ @product = Product.new
+end
 
-  def edit
-  	@product = Product.find(params[:id])
-  end
+def edit
+ @product = Product.find(params[:id])
+end
 
-  def create
-  	@product = Product.new(product_params)
+def create
+ @product = Product.new(product_params)
 
-  	if @product.save
-  		redirect_to products_url
-  	else
-  		render :new
-  	end
-  end
+ if @product.save
+  redirect_to products_url
+else
+  render :new
+end
+end
 
-  def update
-  	@product = Product.find(params[:id])
-  	if @product
-  		@product.update_attributes(product_params)
-  		redirect_to product_path(@product)
-  	else
-  		render :edit
-  	end
-  end
+def update
+ @product = Product.find(params[:id])
+ if @product
+  @product.update_attributes(product_params)
+  redirect_to product_path(@product)
+else
+  render :edit
+end
+end
 
-  def destroy
-  	@product = Product.find(params[:id])
-  	@product.destroy
-  	redirect_to products_path
-  end
+def destroy
+ @product = Product.find(params[:id])
+ @product.destroy
+ redirect_to products_path
+end
 
-  private
-  def product_params
-  	params.require(:product).permit(:name, :description, :price_in_cents, :category_id)
-  end
+private
+def product_params
+ params.require(:product).permit(:name, :description, :price_in_cents, :category_id)
+end
 end
